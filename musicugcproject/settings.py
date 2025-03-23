0bf5fd5171f357512c9f4bdd4af4f38e46004146
wanjosbest <best9ja1@gmail.com>
2025-03-22 21:31:34 -0700

    fourth commit

--- musicugcproject/settings.py
+++ musicugcproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+# load_dotenv()
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-^p#n7%thxxjw07$()e)c4rn#ic=3=d(+ju403+&c^gs18v#lh7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
